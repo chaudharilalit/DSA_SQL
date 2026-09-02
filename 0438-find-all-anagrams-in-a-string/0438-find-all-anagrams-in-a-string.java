253b7f2d392aa5bb228cc3023e6222eeff8f7453
Lalit chaudhari <chaudharilalit064@gmail.com>
2026-09-02 13:11:40 +0530

    Time: 6 ms (97.5%), Space: 46.9 MB (58.17%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -2,40 +2,48 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
        int k = p.length(); 
        int arr[] = new int[26];
+        ArrayList<Integer>list = new ArrayList<>();
+        if (p.length() > s.length()) {
+            return list;
+        }
        for(int i=0 ; i<p.length() ; i++) {
         arr[p.charAt(i) - 'a']++;
        }
 
-       ArrayList<Integer>list = new ArrayList<>();
+     
        int right = 0; 
        int left = 0;
        int hash[] = new int[26];
-       while(left <= s.length()-k ){
-        boolean flag = true;
-          hash[s.charAt(right) - 'a']++;
-           if((right-left+1) == k){
-          
-            for(int i=0 ; i<26 ; i++){
-            if(hash[i] != arr[i]){
-             flag = false;
-            }
-            }
-               if(flag == true) list.add(left);
-               hash[s.charAt(left) - 'a']--;
-               left++;
-           }
-        
-           right++;
-    
+      while(right < k){
+        hash[s.charAt(right) - 'a']++;
+        right++;
+      }
+       boolean flag = true;
+        for(int i=0 ; i<26 ; i++){
+        if(hash[i] != arr[i]){
+            flag = false;
+        }
        }
-    //    boolean flag = true;
-    //     for(int i=0 ; i<26 ; i++){
-    //         if(hash[i] != arr[i]){
-    //          flag = false;
-    //         }
-    //         }
-    //            if(flag == true) list.add(left);
-       
+      if( flag == true )list.add(left);
+
+
+      while(right < s.length()){
+        hash[s.charAt(left) - 'a']--;
+        hash[s.charAt(right)- 'a']++;
+        
+        left++;
+        right++;
+
+     flag = true;
+      for(int i=0 ; i<26 ; i++){
+        if(hash[i] != arr[i]){
+            flag = false;
+        }
+      }
+      
+      if(flag == true)list.add(left);
+
+      }
        return list;
     }
 }
